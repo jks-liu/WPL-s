3dcb7e689a5727f3a088d647bbc90d899df21ef7
Jks Liu <chinatianma@gmail.com>
2021-08-02 20:44:29 +0800

    Disable eslint rule of no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,10 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+        // note you must disable the base rule as it can report incorrect errors
+        "no-unused-vars": "off",
+        // FIXME: Also false positive, disables the rule. Opt in when it works
+        // "@typescript-eslint/no-unused-vars": ["error"]
     }
 };
